8173b7691c14c566bf9f7445be3dd5c811621b81
anatoly.stupin <anatoly.stupin@roonyx.team>
2019-09-01 20:57:49 +0300

    Add: models tests

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+Rspec.describe Person, type: model do
+  it { should have_many(:declensions).dependent(:destroy) }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_inclusion_of(:sex).in_array(%w(male female))}
+end
